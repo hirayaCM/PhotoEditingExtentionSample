13bd8f57013052c72169c79dfb2deb71e7d4c447
Shingo Hiraya <hiraya.shingo@classmethod.jp>
2014-11-26 18:28:07 +0900

    Initial Commit

---
+++ PhotoEditingExtensionSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoEditingExtensionSample
+//
+//  Created by hiraya.shingo on 2014/11/26.
+//  Copyright (c) 2014年 hiraya.shingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoEditingExtensionSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoEditingExtensionSample
+//
+//  Created by hiraya.shingo on 2014/11/26.
+//  Copyright (c) 2014年 hiraya.shingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
